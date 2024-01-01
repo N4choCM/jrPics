a2464cbbb388743cc6472e9a5c909d23b2c6ef30
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2024-01-01 20:13:18 +0100

    ✅ ADDED: DarkMode in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,33 +3,48 @@ import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
 import logo from "../../assets/logo.svg";
 import PropTypes from "prop-types";
-import Swal from 'sweetalert2';
-import { Home20Regular, PersonInfo20Regular, Camera20Regular, WeatherSunny20Regular, WeatherMoon20Regular } from '@fluentui/react-icons';
+import Swal from "sweetalert2";
+import {
+	Home20Regular,
+	PersonInfo20Regular,
+	Camera20Regular,
+	WeatherSunny20Regular,
+	WeatherMoon20Regular,
+} from "@fluentui/react-icons";
 import { FaInstagram, FaFacebookSquare } from "react-icons/fa";
 import { GrContact } from "react-icons/gr";
+import { useContext } from "react";
+import { AppStateContext } from "../../state/AppProvider";
 
-const NavBar = ({ darkMode, changeDarkMode }) => {
-
-	darkMode = true;
+const NavBar = () => {
+	const appStateContext = useContext(AppStateContext);
 
 	const showContactInfo = () => {
 		Swal.fire({
 			html: `
-				<h6>Email: nachocamposdev@gmail.com</h6>
-				<h6>Phone: +34 628 523 682</h6>
+				<h6>Email: jrfln4@yahoo.es</h6>
 			`,
-			icon: 'info',
+			icon: "info",
 			showCancelButton: false,
 			showConfirmButton: false,
 			showCloseButton: true,
-			position: window.innerWidth <= 992 ? 'center' : 'top-end',
+			position: window.innerWidth <= 992 ? "center" : "top-end",
 		});
 	};
-	
+
+	const handleDarkModeClick = () => {
+		appStateContext?.dispatch({ type: "TOGGLE_DARK_MODE" });
+	};
 
 	return (
 		<header className="fixed-top">
-			<nav className="navbar navbar-expand-lg navbar-dark bg-navbar-light">
+			<nav
+				className={`navbar navbar-expand-lg navbar-dark ${
+					appStateContext?.state.isDarkMode
+						? "bg-navbar-dark"
+						: "bg-navbar-light"
+				}`}
+			>
 				<div className="container">
 					<Link
 						to="home"
@@ -41,8 +56,8 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 						<div>
 							<img
 								className={
-									!darkMode
-										? "img-logo img-logo-white me-3"
+									!appStateContext?.state.isDarkMode
+										? "img-logo me-3"
 										: "img-logo me-3"
 								}
 								src={logo}
@@ -51,7 +66,7 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 							<span>
 								<h4
 									className={
-										darkMode
+										!appStateContext?.state.isDarkMode
 											? "text-dark d-none d-md-block"
 											: "text-white d-none d-md-block"
 									}
@@ -60,7 +75,7 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 								</h4>
 								<h6
 									className={
-										darkMode
+										!appStateContext?.state.isDarkMode
 											? "text-dark text-center d-none d-md-block"
 											: "text-white text-center d-none d-md-block"
 									}
@@ -83,7 +98,13 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 					</button>
 					<div className="collapse navbar-collapse" id="navbarNav">
 						<ul className="navbar-nav ms-auto">
-							<li className="nav-item hover-custom">
+							<li
+								className={`nav-item ${
+									appStateContext?.state.isDarkMode
+										? "hover-custom-dark"
+										: "hover-custom-light"
+								}`}
+							>
 								<Link
 									to="home"
 									smooth={true}
@@ -91,14 +112,16 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 									activeClassName="active"
 									className="nav-link custom-pointer"
 								>
-									<Home20Regular className={
-											darkMode
+									<Home20Regular
+										className={
+											!appStateContext?.state.isDarkMode
 												? "text-dark me-1"
 												: "text-white me-1"
-										} />
+										}
+									/>
 									<span
 										className={
-											darkMode
+											!appStateContext?.state.isDarkMode
 												? "text-dark"
 												: "text-white"
 										}
@@ -107,7 +130,13 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 									</span>
 								</Link>
 							</li>
-							<li className="nav-item hover-custom">
+							<li
+								className={`nav-item ${
+									appStateContext?.state.isDarkMode
+										? "hover-custom-dark"
+										: "hover-custom-light"
+								}`}
+							>
 								<Link
 									to="about"
 									smooth={true}
@@ -115,14 +144,16 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 									activeClassName="active"
 									className="nav-link custom-pointer"
 								>
-									<PersonInfo20Regular className={
-											darkMode
+									<PersonInfo20Regular
+										className={
+											!appStateContext?.state.isDarkMode
 												? "text-dark me-1"
 												: "text-white me-1"
-										} />
+										}
+									/>
 									<span
 										className={
-											darkMode
+											!appStateContext?.state.isDarkMode
 												? "text-dark"
 												: "text-white"
 										}
@@ -131,23 +162,38 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 									</span>{" "}
 								</Link>
 							</li>
-							<li className="nav-item hover-custom">
+							<li
+								className={`nav-item dropdown ${
+									appStateContext?.state.isDarkMode
+										? "hover-custom-dark"
+										: "hover-custom-light"
+								}`}
+							>
 								<Link
 									to="projects"
 									smooth={true}
 									duration={200}
 									activeClassName="active"
-									className="nav-link custom-pointer"
+									className={`nav-link dropdown-toggle custom-pointer ${
+										appStateContext?.state.isDarkMode
+											? "text-white"
+											: "text-dark"
+									}`}
+									role="button"
+									data-bs-toggle="dropdown"
+									aria-expanded="false"
 								>
 									{" "}
-									<Camera20Regular className={
-											darkMode
+									<Camera20Regular
+										className={
+											!appStateContext?.state.isDarkMode
 												? "text-dark me-1"
 												: "text-white me-1"
-										} />
+										}
+									/>
 									<span
 										className={
-											darkMode
+											!appStateContext?.state.isDarkMode
 												? "text-dark"
 												: "text-white"
 										}
@@ -155,22 +201,123 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 										Categorías
 									</span>{" "}
 								</Link>
+								<ul
+									className={`dropdown-menu ${
+										appStateContext?.state.isDarkMode
+											? "bg-navbar-dark"
+											: "bg-navbar-light"
+									}`}
+								>
+									<li>
+										<Link
+											className={`dropdown-item ${
+												appStateContext?.state.isDarkMode
+													? "hover-custom-dark text-white"
+													: "hover-custom-light"
+											}`}
+											to="#"
+											activeClassName="active"
+											role="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false"
+										>
+											Playa
+										</Link>
+									</li>
+									<li>
+										<hr
+											className={`dropdown-divider ${
+												appStateContext?.state.isDarkMode ? "cm-hr-light" : ""
+											}`}
+										/>
+									</li>
+									<li>
+										<Link
+											className={`dropdown-item ${
+												appStateContext?.state.isDarkMode
+													? "hover-custom-dark text-white"
+													: "hover-custom-light"
+											}`}
+											to="#"
+											activeClassName="active"
+											role="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false"
+										>
+											Viajes
+										</Link>
+									</li>
+									<li>
+										<hr
+											className={`dropdown-divider ${
+												appStateContext?.state.isDarkMode ? "cm-hr-light" : ""
+											}`}
+										/>
+									</li>
+									<li>
+										<Link
+											className={`dropdown-item ${
+												appStateContext?.state.isDarkMode
+													? "hover-custom-dark text-white"
+													: "hover-custom-light"
+											}`}
+											to="#"
+											activeClassName="active"
+											role="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false"
+										>
+											Comida
+										</Link>
+									</li>
+									<li>
+										<hr
+											className={`dropdown-divider ${
+												appStateContext?.state.isDarkMode ? "cm-hr-light" : ""
+											}`}
+										/>
+									</li>
+									<li>
+										<Link
+											className={`dropdown-item ${
+												appStateContext?.state.isDarkMode
+													? "hover-custom-dark text-white"
+													: "hover-custom-light"
+											}`}
+											to="#"
+											activeClassName="active"
+											role="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false"
+										>
+											Deporte
+										</Link>
+									</li>
+								</ul>
 							</li>
-							<li className="nav-item hover-custom">
+							<li
+								className={`nav-item ${
+									appStateContext?.state.isDarkMode
+										? "hover-custom-dark"
+										: "hover-custom-light"
+								}`}
+							>
 								<NavLink
 									className="nav-link"
 									aria-current="page"
-									to="https://github.com/N4choCM"
+									to="https://www.instagram.com/juanramon.campos/"
 									target="_blank"
 								>
-									<FaInstagram className={
-											darkMode
+									<FaInstagram
+										className={
+											!appStateContext?.state.isDarkMode
 												? "text-dark fa fa-github me-1"
 												: "text-white fa fa-github me-1"
-										} />
+										}
+									/>
 									<span
 										className={
-											darkMode
+											!appStateContext?.state.isDarkMode
 												? "text-dark"
 												: "text-white"
 										}
@@ -179,21 +326,29 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 									</span>{" "}
 								</NavLink>
 							</li>
-							<li className="nav-item hover-custom">
+							<li
+								className={`nav-item ${
+									appStateContext?.state.isDarkMode
+										? "hover-custom-dark"
+										: "hover-custom-light"
+								}`}
+							>
 								<NavLink
 									className="nav-link"
 									aria-current="page"
-									to="https://www.linkedin.com/in/ignacio-campos-marti/"
+									to="https://www.facebook.com/profile.php?id=100087924643423"
 									target="_blank"
 								>
-									<FaFacebookSquare className={
-											darkMode
+									<FaFacebookSquare
+										className={
+											!appStateContext?.state.isDarkMode
 												? "text-dark fa fa-github me-1"
 												: "text-white fa fa-github me-1"
-										} />
+										}
+									/>
 									<span
 										className={
-											darkMode
+											!appStateContext?.state.isDarkMode
 												? "text-dark"
 												: "text-white"
 										}
@@ -202,42 +357,51 @@ const NavBar = ({ darkMode, changeDarkMode }) => {
 									</span>{" "}
 								</NavLink>
 							</li>
-							<li className="nav-item hover-custom">
-            <NavLink
-              className="nav-link"
-              aria-current="page"
-              to="#"
-              onClick={showContactInfo}
-            >
-							<GrContact className={
-											darkMode
+							<li
+								className={`nav-item ${
+									appStateContext?.state.isDarkMode
+										? "hover-custom-dark"
+										: "hover-custom-light"
+								}`}
+							>
+								<NavLink
+									className="nav-link"
+									aria-current="page"
+									to="#"
+									onClick={showContactInfo}
+								>
+									<GrContact
+										className={
+											!appStateContext?.state.isDarkMode
 												? "text-dark fa fa-github me-1"
 												: "text-white fa fa-github me-1"
-										} />
-              <span
-                className={
-                  darkMode
-                    ? "text-dark"
-                    : "text-white"
-                }
-              >
-                Contacto
-              </span>
-            </NavLink>
-          </li>
+										}
+									/>
+									<span
+										className={
+											!appStateContext?.state.isDarkMode
+												? "text-dark"
+												: "text-white"
+										}
+									>
+										Contacto
+									</span>
+								</NavLink>
+							</li>
 						</ul>
 						<div className="d-flex gap-2 align-items-center justify-content-center ms-2">
-							{darkMode ? (
-								<WeatherSunny20Regular className="" />
+							{appStateContext?.state.isDarkMode ? (
+								<WeatherSunny20Regular className="text-light" />
 							) : (
 								<WeatherMoon20Regular />
 							)}
 							<div className="form-check form-switch">
 								<input
-									className="form-check-input"
+									className="form-check-input cm-pointer"
 									type="checkbox"
 									role="switch"
-									onChange={changeDarkMode}
+									onChange={handleDarkModeClick}
+									checked={!appStateContext?.state.isDarkMode}
 								/>
 							</div>
 						</div>
